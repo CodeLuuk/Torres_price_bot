e863adcb2b713632c7e19aa9fa1f0bf1f4063dc0
CodeLuuk <luukscheers162@gmail.com>
2025-11-22 18:41:55 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,12 +19,9 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
-    def send_telegram(message):
-        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-        payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
 
-        response = requests.post(url, data=payload)
-        print("TELEGRAM RESPONSE:", response.text)
+    response = requests.post(url, data=payload)
+    print("TELEGRAM RESPONSE:", response.text)
 
 
 def check_price(url):
